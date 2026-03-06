e017b94d0a37e2c9ab85e3f187efa16e3587293f
David Olafson <dave@vikinghelmet.com>
2026-03-06 16:59:10 -0500

    action logging

---
+++ app/src/main/kotlin/com/vikinghelmet/dnd/dpr/character/actions/Action.kt
@@ -0,0 +1,9 @@
+package com.vikinghelmet.dnd.dpr.character.actions
+
+enum class Action(val traitName: String) {
+    HuntersLore("Hunter's Lore"),
+    HordeBreaker("Horde Breaker"),
+    ColossusSlayer("Colossus Slayer"),
+    DreadfulStrike("Dreadful Strike"),
+    PolarStrikes("Polar Strikes"),
+}
