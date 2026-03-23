6f13e78449c73714702d3af2d327e6c66d1464c7
David Olafson <dave@vikinghelmet.com>
2026-03-23 15:28:31 -0400

    improve enter-key handling / autocomplete on monster menu

--- composeApp/src/commonMain/kotlin/com/vikinghelmet/dnd/dprapp/ui/widgets/MonsterMenu.kt
+++ composeApp/src/commonMain/kotlin/com/vikinghelmet/dnd/dprapp/ui/widgets/MonsterMenu.kt
@@ -1,10 +1,17 @@
 package com.vikinghelmet.dnd.dprapp.ui.widgets
 
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.text.input.TextFieldLineLimits
 import androidx.compose.foundation.text.input.TextFieldState
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.key.Key
+import androidx.compose.ui.input.key.key
+import androidx.compose.ui.input.key.onKeyEvent
+import androidx.compose.ui.input.key.utf16CodePoint
+import androidx.compose.ui.text.input.ImeAction
 import com.vikinghelmet.dnd.dpr.monsters.Monster
 import com.vikinghelmet.dnd.dpr.util.Globals
 
@@ -16,22 +23,53 @@ fun MonsterMenu(textFieldState: TextFieldState, fillMaxWidth: Boolean, onMenuIte
     var expanded by remember { mutableStateOf(false) }
     //val textFieldState = rememberTextFieldState()
 
+    // filter options based on text field value
+    val filteringOptions =
+        options.filter { it.contains(textFieldState.text, ignoreCase = true) }
+
     ExposedDropdownMenuBox(expanded = expanded, onExpandedChange = { expanded = it }) {
         TextField(
             state = textFieldState,
+            lineLimits = TextFieldLineLimits.SingleLine,
             label = { Text("Select Monster") },
             readOnly = false,
             trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded,
                 modifier = Modifier.menuAnchor(ExposedDropdownMenuAnchorType.SecondaryEditable))
             },
             colors = ExposedDropdownMenuDefaults.textFieldColors(),
-            modifier = if (!fillMaxWidth) Modifier.menuAnchor(MenuAnchorType.PrimaryEditable)
-                        else              Modifier.fillMaxWidth().menuAnchor(MenuAnchorType.PrimaryEditable)
-        )
+            //modifier = if (!fillMaxWidth) Modifier.menuAnchor(MenuAnchorType.PrimaryEditable)
+            //            else              Modifier.fillMaxWidth().menuAnchor(MenuAnchorType.PrimaryEditable)
 
-        // filter options based on text field value
-        val filteringOptions =
-            options.filter { it.contains(textFieldState.text, ignoreCase = true) }
+            modifier = getModifier(fillMaxWidth).menuAnchor(MenuAnchorType.PrimaryEditable)
+                // handle Return key for Desktop
+                .onKeyEvent { keyEvent ->
+                    expanded = true
+
+                    // for enter key, look for a single selection, or a full-text match (ignoring case)
+                    val selection = if (filteringOptions.size == 1) filteringOptions[0]
+                        else filteringOptions.firstOrNull { it.equals(textFieldState.text.toString(), ignoreCase = true) }
+
+                    // println ("keyEvent, key  = ${keyEvent.key}")
+                    // println ("keyEvent, utf  = ${keyEvent.utf16CodePoint}")
+                    // println ("keyEvent, type = ${keyEvent.type}")
+                    // println ("keyEvent, native = ${keyEvent.nativeKeyEvent}")
+
+                    if (selection != null && (keyEvent.key == Key.Enter || keyEvent.utf16CodePoint == 10)) {
+                        //println ("enterKey: firstOption: $firstOption, currentText: $currentText, filteringOptions: $filteringOptions")
+                        onMenuItemSelected(Globals.getMonsterOrNull(selection))
+                        true // Event handled
+                    } else {
+                        false // Event propagated
+                    }
+                } ,
+            // handle Return key for Mobile
+            keyboardOptions  = KeyboardOptions( imeAction = ImeAction.Done ),
+            onKeyboardAction = {
+                if (filteringOptions.size == 1) {
+                    onMenuItemSelected(Globals.getMonsterOrNull(filteringOptions[0]))
+                }
+            }
+        )
 
         if (filteringOptions.isNotEmpty()) {
             ExposedDropdownMenu(expanded = expanded, onDismissRequest = { expanded = false }) {
@@ -47,4 +85,8 @@ fun MonsterMenu(textFieldState: TextFieldState, fillMaxWidth: Boolean, onMenuIte
             }
         }
     }
+}
+
+fun getModifier(fillMaxWidth: Boolean): Modifier {
+    return if (fillMaxWidth) Modifier.fillMaxWidth() else Modifier
 }
