96ada560f2b75708080f39d8133d8b7f5a243408
David Olafson <dave@vikinghelmet.com>
2026-04-12 22:27:23 -0400

    add tests for 'best result' (highest dpr) for melee/range over 1-3 turns

---
+++ dprlib/src/jvmTest/kotlin/com.vikinghelmet.dnd.dpr.character/ScenarioCalculatorTest.kt
@@ -0,0 +1,73 @@
+package com.vikinghelmet.dnd.dpr.character
+
+import com.vikinghelmet.dnd.dpr.scenario.ScenarioBuilder
+import com.vikinghelmet.dnd.dpr.scenario.ScenarioCalculator
+import com.vikinghelmet.dnd.dpr.scenario.ScenarioResult
+import com.vikinghelmet.dnd.dpr.util.Constants
+import com.vikinghelmet.dnd.dpr.util.Constants.DEFAULT_NUM_TARGETS
+import com.vikinghelmet.dnd.dpr.util.Constants.DEFAULT_TARGET_RADIUS
+import com.vikinghelmet.dnd.dpr.util.Globals
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class ScenarioCalculatorTest {
+
+    @Test
+    fun scenarioCalculatorBestResultMeleeOneTurn() {
+        val scenarioList = ScenarioBuilder(TestUtil.leif, Globals.getMonster("Goblin"))
+            .build(Constants.MELEE_RANGE, 1, DEFAULT_NUM_TARGETS, DEFAULT_TARGET_RADIUS)
+
+        val scenarioResultList = scenarioList.map { ScenarioCalculator(it).calculateDPRForAllTurns() }.toList()
+        val topResult = ScenarioResult.topResults(scenarioResultList, 1)[0]
+
+        assertEquals(listOf("Shortsword","Shortsword"), topResult.scenario.turns[0].attacks.map { it.getLabel() } )
+    }
+
+    @Test
+    fun scenarioCalculatorBestResultRangeOneTurn() {
+        val scenarioList = ScenarioBuilder(TestUtil.leif, Globals.getMonster("Goblin"))
+            .build(Constants.MELEE_RANGE*2, 1, DEFAULT_NUM_TARGETS, DEFAULT_TARGET_RADIUS)
+
+        val scenarioResultList = scenarioList.map { ScenarioCalculator(it).calculateDPRForAllTurns() }.toList()
+        val topResult = ScenarioResult.topResults(scenarioResultList, 1)[0]
+
+        assertEquals(listOf("Longbow","Hail of Thorns"), topResult.scenario.turns[0].attacks.map { it.getLabel() } )
+    }
+    
+    @Test
+    fun scenarioCalculatorBestResultMeleeTwoTurns() {
+        val scenarioList = ScenarioBuilder(TestUtil.leif, Globals.getMonster("Goblin"))
+            .build(Constants.MELEE_RANGE, 2, DEFAULT_NUM_TARGETS, DEFAULT_TARGET_RADIUS)
+
+        val scenarioResultList = scenarioList.map { ScenarioCalculator(it).calculateDPRForAllTurns() }.toList()
+        val topResult = ScenarioResult.topResults(scenarioResultList, 1)[0]
+
+        assertEquals(listOf("Dagger","Hunter's Mark"),  topResult.scenario.turns[0].attacks.map { it.getLabel() } )
+        assertEquals(listOf("Shortsword","Shortsword"), topResult.scenario.turns[1].attacks.map { it.getLabel() } )
+    }
+
+    @Test
+    fun scenarioCalculatorBestResultRangeTwoTurns() {
+        val scenarioList = ScenarioBuilder(TestUtil.leif, Globals.getMonster("Goblin"))
+            .build(Constants.MELEE_RANGE*2, 2, DEFAULT_NUM_TARGETS, DEFAULT_TARGET_RADIUS)
+
+        val scenarioResultList = scenarioList.map { ScenarioCalculator(it).calculateDPRForAllTurns() }.toList()
+        val topResult = ScenarioResult.topResults(scenarioResultList, 1)[0]
+
+        assertEquals(listOf("Longbow","Hail of Thorns"), topResult.scenario.turns[0].attacks.map { it.getLabel() } )
+        assertEquals(listOf("Longbow","Hail of Thorns"), topResult.scenario.turns[1].attacks.map { it.getLabel() } )
+    }
+
+    @Test
+    fun scenarioCalculatorBestResultRangeThreeTurns() {
+        val scenarioList = ScenarioBuilder(TestUtil.leif, Globals.getMonster("Goblin"))
+            .build(Constants.MELEE_RANGE*2, 3, DEFAULT_NUM_TARGETS, DEFAULT_TARGET_RADIUS)
+
+        val scenarioResultList = scenarioList.map { ScenarioCalculator(it).calculateDPRForAllTurns() }.toList()
+        val topResult = ScenarioResult.topResults(scenarioResultList, 1)[0]
+
+        assertEquals(listOf("Longbow","Hunter's Mark"), topResult.scenario.turns[0].attacks.map { it.getLabel() } )
+        assertEquals(listOf("Longbow","Hail of Thorns"), topResult.scenario.turns[1].attacks.map { it.getLabel() } )
+        assertEquals(listOf("Longbow","Hail of Thorns"), topResult.scenario.turns[2].attacks.map { it.getLabel() } )
+    }
+}
